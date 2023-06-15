13d0aaada1aa2e10af44da49fbf52298d6b20d9d
luccasst <lucas.c.r.f@hotmail.com>
2023-06-15 14:09:10 -0300

    initial commit

---
+++ crudUser/model/user.cs
@@ -0,0 +1,9 @@
+namespace SistemaDeTarefas.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
